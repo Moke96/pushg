54806ceb835b54859300c9eef6d43c8b5f6b50e3
Maurice <maurice.matuschek@hotmail.de>
2019-08-17 02:19:35 +0200

    Api call funktioniert, api calls aus dem frontend werden nicht zugelassen = brauchen ein backend

--- js/leagueAPIAccess.js
+++ js/leagueAPIAccess.js
@@ -1,15 +1,18 @@
 const accIDURL = ".api.riotgames.com/lol/summoner/v4/summoners/by-name/";
 const api_key = "RGAPI-d907c69f-4d23-4468-8956-4bd83be18564";
-const data={};
-const header = {
+const api_key_url = "?api_key=" + api_key;
+
+const Leagueheader = {
     "Origin": null,
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
     "X-Riot-Token": api_key,
-}
+};
 async function getAccID(){
-    requestURL = "https://" + "euw1" + accIDURL + "OnlyGlutax";
+    let region = $('#region').val();
+    let name = $('#summoner_name').val();
+    requestURL = "https://" + region + accIDURL + name + api_key_url;
     let parameter= {
-        headers: header,
+        //headers: header,
         method:"GET"
     };
     await fetch(requestURL,parameter)
@@ -18,5 +21,4 @@ async function getAccID(){
             console.log(res);
             result = res;
         });
-
 }
