a76855d818c7d11285bd96018c1f40e9b06ac789
Skaze4k <31975486+Skaze4k@users.noreply.github.com>
2019-08-21 14:30:07 +0200

    api calls refactor

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const aram = require('./js/aram');
 const pubg = require('./js/pubg');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -9,9 +10,12 @@ app.get('/', function (req, res) {
     res.render('index', {stat: null});
 });
 
-app.post('/aram', function (req, res) {
-    const aram = require('./js/aram');
-    aram.getAccID()
+app.post('/aram', async function (req, res) {
+    let region = req.body.region;
+    let player_name = req.body.playername;
+
+    let data = await aram.writeList(region, player_name);
+    res.render('aram', {stat: data})
 });
 
 app.post('/pubg', async function (req, res) {
--- js/aram.js
+++ js/aram.js
@@ -1,11 +1,13 @@
-let region = req.body.region;
-let name = req.body.playername;
-const baseURL = "https://" + region + ".api.riotgames.com/lol/";
+const fetch = require('node-fetch');
+
+let baseURL = '';
 const accIDURL = "summoner/v4/summoners/by-name/";
 const matchesByAccountIDURL = "match/v4/matchlists/by-account/";
 const matchByMatchIDURL = "match/v4/matches/";
-const api_key = "RGAPI-a4ae2208-ccd0-46bd-8078-f408eed12523";
+const api_key = "RGAPI-1a3db87c-72d8-4d73-b02d-13e261a73675";
 const api_key_url = "api_key=" + api_key;
+
+let list = '';
 
 const Leagueheader = {
     "Origin": null,
@@ -15,10 +17,11 @@ const Leagueheader = {
 
 let gloabalresult = {};
 
-async function getAccID() {
+async function getAccID(region, player_name) {
+    baseURL = "https://" + region + ".api.riotgames.com/lol/";
     let result = {};
 
-    let requestURL = baseURL + accIDURL + name + "?" + api_key_url;
+    let requestURL = baseURL + accIDURL + player_name + "?" + api_key_url;
     let parameter = {
         //headers: header,
         method: "GET"
@@ -27,16 +30,16 @@ async function getAccID() {
     await fetch(requestURL, parameter)
         .then(data => data.json())
         .then(res => {
-            console.log(res);
+            //console.log(res);
             result = res;
         });
     return result.accountId;
 }
 
-async function getLastMatch() {
+async function getLastMatch(region, player_name) {
     let result = {};
     let lastMatch = {};
-    await getAccID().then(async id => {
+    await getAccID(region, player_name).then(async id => {
         let queueURL = "?queue=450"; //nur ARAM
         let requestURL = baseURL + matchesByAccountIDURL + id + queueURL + "&" + api_key_url;
         let parameter = {
@@ -45,16 +48,16 @@ async function getLastMatch() {
         await fetch(requestURL, parameter)
             .then(data => data.json())
             .then(res => {
-                console.log(res);
+                //console.log(res);
                 lastMatch = res.matches[0];
             });
 
     });
     return lastMatch;
 }
-async function getMatchStats() {
+async function getMatchStats(region, player_name) {
     let result = {};
-    await  getLastMatch().then(async match => {
+    await  getLastMatch(region, player_name).then(async match => {
         let matchID = match.gameId;
         let requestURL = baseURL + matchByMatchIDURL + matchID + "?" + api_key_url;
         let parameter = {
@@ -64,19 +67,19 @@ async function getMatchStats() {
         await fetch(requestURL,parameter)
             .then(data => data.json())
             .then(res => {
-                console.log(res);
+                //console.log(res);
                 result = res;
             });
 
     })
     return result;
 }
-async function getLastMatchStatsFromPlayer(){
+async function getLastMatchStatsFromPlayer(region, player_name){
     let result = {};
-    await getMatchStats().then(async match => {
+    await getMatchStats(region, player_name).then(async match => {
         let players = match.participantIdentities;
         for (i=0;i<players.length;i++){
-            if (players[i].player !== undefined && players[i].player.summonerName===name){
+            if (players[i].player !== undefined && players[i].player.summonerName===player_name){
                 var id = players[i].participantId
                 break;
             }
@@ -85,13 +88,14 @@ async function getLastMatchStatsFromPlayer(){
     });
     return result;
 }
-async function writeStatList() {
-    await getLastMatchStatsFromPlayer().then(async stats => {
+
+
+exports.writeList = async function writeList(region, player_name) {
+    await getLastMatchStatsFromPlayer(region, player_name).then(async stats => {
         list = "";
         for (s in stats) {
             list += "  <li class=\"list-group-item\">" + s + ": " + stats[s] + "</li>"
         }
-        res.render('aram', {stat: list})
-    })
-}
-writeStatList();
+    });
+    return list;
+};
